be966b237a37dfef45d2f2182268e97a2e66bb85
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-08-13 16:58:53 -0300

    ajuste para aceitar rem 10 == 0

--- lib/despachante.ex
+++ lib/despachante.ex
@@ -1,17 +1,37 @@
 defmodule Despachante do
   @moduledoc """
-  Biblioteca para validação de documentos brasileiros.
+  Utils for validation, mocking and generation of brazilian document information.
 
-     iex Despachante.valida_cpf("111") == true
+    iex>  Despachante.valid?(:cpf, "081.495.660-23")
+    true
   """
-  def _validate_digit(digits, validate_array, digit) do
-    digits
-    |> Enum.zip_reduce(validate_array, 0, fn left, right, acc -> acc + (left * right) end)
-    |> Kernel.*(10)
-    |> Kernel.rem(11)
-    |> Kernel.==(digit)
+  defp validate_digit(digits, validate_array, digit) do
+    remainder = digits
+                |> Enum.zip_reduce(validate_array, 0, fn left, right, acc -> acc + (left * right) end)
+                |> Kernel.*(10)
+                |> Kernel.rem(11)
+
+    remainder == digit or (remainder == 10 and digit == 0)
   end
 
+  @doc """
+  Function that returns if the required document is valid. 
+
+  Despachante.valid?(document, document_number) where: 
+    document is one of 
+    | :cpf for Cadastro de Pessoa Física (CPF) validation 
+    | :cnpj for Cadastro Nacional de Pessoa Jurídica (CNPJ) validation
+    document_number is the value to be validated 
+
+    iex> Despachante.valid?(:cpf, "825.205.780-25")
+    true 
+
+    iex> Despachante.valid?(:cpf, "39549381030")
+    true 
+
+    iex> Despachante.valid?(:cpf, "946.502.280-25")
+    false
+  """
   def valid?(:cpf, document_number) do
     cpf = Regex.replace(~r/[^0-9]/, document_number, "")
           |> String.split("", trim: true)
@@ -22,11 +42,11 @@ defmodule Despachante do
 
     is_first_digit_valid? = cpf
                             |> Enum.slice(0, 9)
-                            |> _validate_digit(Enum.to_list(10..2), List.first(digits)) 
+                            |> validate_digit(Enum.to_list(10..2), List.first(digits)) 
 
     is_second_digit_valid? = cpf
                             |> Enum.slice(0, 10)
-                            |> _validate_digit(Enum.to_list(11..2), List.last(digits)) 
+                            |> validate_digit(Enum.to_list(11..2), List.last(digits)) 
 
     is_first_digit_valid? and is_second_digit_valid?
   end
