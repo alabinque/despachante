29c456392f3ca4bf0ce1d9e080604d1c2c215fbd
Camilo Cunha de Azevedo <camilotk@gmail.com>
2022-08-14 17:54:17 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Despachante.MixProject do
     [
       app: :despachante,
       version: "0.1.0",
-      elixir: "1.13.4",
+      elixir: "~1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
